122949a7f0459f4f20a58926f8a5881823216995
BulichevMaks <formgrav@mail.ru>
2023-01-18 15:12:24 +0100

    second commit in dev, remove foo() from NoteMenu,

--- src/main/kotlin/NoteMenu.kt
+++ src/main/kotlin/NoteMenu.kt
@@ -6,14 +6,7 @@ class NoteMenu(private val content: String) {
     fun crateMenu() {
         while (true) {
             println("Тест заметки: $content")
-            if (screen.createScreen(foo())) return
+            if (screen.createScreen(mutableMapOf())) return
         }
-    }
-
-    //Создает набор для исполнения функций при выборе пункта меню
-    private fun foo(): MutableMap<Int, () -> Unit> {
-        val menu = mutableMapOf<Int, () -> Unit>()
-        menu[0] = {}//создаёт одно значение чтобы переменная exit не имела значение 0 :-)
-        return menu
     }
 }
